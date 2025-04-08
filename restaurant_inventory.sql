2181a738728bb7920771f68c01a55da3254ee428
Ilan-Goren <99876458+Ilan-Goren@users.noreply.github.com>
2025-04-08 11:58:47 +0100

    updated database

--- restaurant_inventory.sql
+++ restaurant_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 25, 2025 at 12:19 PM
+-- Generation Time: Apr 08, 2025 at 12:30 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
